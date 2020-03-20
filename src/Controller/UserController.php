db88e22f345a83d590e237347862c46d8fd97b87
Nessim Ben Salha <nbensalha@outlook.com>
2020-03-20 13:12:02 +0100

    [USER] finished setting up rudimentary user controller

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,25 +2,128 @@
 
 namespace App\Controller;
 
+use Exception;
+use Twig\Environment;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Form\FormFactoryInterface;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\RouterInterface;
+
+use App\Entity\User;
+use App\Form\UserType;
+
+/**
+ * @Route("/user", name="user_")
+ */
 class UserController
 {
-    public function view()
-    {
+    /**
+     * @var Environment
+     */
+    private $twig;
 
+    /**
+     * @var EntityManagerInterface
+     */
+    private $entityManager;
+
+    /**
+     * @var FormFactoryInterface
+     */
+    private $formFactory;
+
+    /**
+     * @var RouterInterface
+     */
+    private $router;
+
+    /**
+     * UserController constructor.
+     * @param Environment $twig
+     * @param EntityManagerInterface $entityManager
+     * @param FormFactoryInterface $formFactory
+     * @param RouterInterface $router
+     */
+    public function __construct(
+        Environment $twig,
+        EntityManagerInterface $entityManager,
+        FormFactoryInterface $formFactory,
+        RouterInterface $router
+    )
+    {
+        $this->twig = $twig;
+        $this->entityManager = $entityManager;
+        $this->formFactory = $formFactory;
+        $this->router = $router;
     }
 
+    /**
+     * @Route("/{id}", name="view", methods={"GET"})
+     * @param User $user
+     * @return string
+     * @throws Exception
+     */
+    public function view(User $user)
+    {
+        return new Response($this->twig->render('user/view.html.twig', [
+            'user' => $user
+        ]));
+    }
+
+    /**
+     * @Route("/new", name="edit", methods={"GET","POST"})
+     * @throws Exception
+     */
     public function create()
     {
-
+        return new Response($this->twig->render('user/create.html.twig'));
     }
 
-    public function edit()
+    /**
+     * @Route("/edit/{id}", name="edit", methods={"GET","POST"})
+     * @param User $user
+     * @return Response|RedirectResponse
+     * @throws Exception
+     */
+    public function edit(User $user)
+    {
+        $form = $this->formFactory->create(UserType::class, $user);
+
+        $form->handleRequest($user);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $user = $form->getData();
+
+            $url = $this->router->generate('user_view', [$user]);
+            return new RedirectResponse($url, 302);
+        }
+
+        return new Response($this->twig->render('user/edit.html.twig', [
+            'user' => $user,
+            'form' => $form->createView(),
+        ]));
+    }
+
+    /**
+     * @Route("/delete/{id}", name="delete", methods={"DELETE"})
+     * @param User $user
+     */
+    public function delete(User $user)
     {
 
     }
 
-    public function delete()
+    /**
+     * @Route("/list", name="list", methods={"GET"})
+     * @throws Exception
+     */
+    public function list()
     {
+        $users = $this->entityManager->getRepository(User::class)->findAll();
 
+        return new Response($this->twig->render('user/list.html.twig', [
+            'users' => $users
+        ]));
     }
 }
---
+++ src/Form/UserType.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+
+class UserType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('username', TextType::class)
+            ->add('password', PasswordType::class);
+    }
+}
