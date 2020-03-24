6b66197336916d72fd9770f1e0ba950773e1e254
kromajmun <remikoci@hotmail.fr>
2020-03-24 23:57:33 +0100

    back button

--- src/Controller/LegalController.php
+++ src/Controller/LegalController.php
@@ -7,6 +7,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Routing\RouterInterface;
+use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
 
 /**
  * @Route("/legal", name="legal_")
@@ -24,17 +25,25 @@ class LegalController
     private $router;
 
     /**
+     * @var AuthorizationCheckerInterface
+     */
+    private $authenticationChecker;
+
+    /**
      * LegalController constructor.
      * @param Environment $twig
      * @param RouterInterface $router
+     * @param AuthorizationCheckerInterface $authenticationChecker
      */
     public function __construct(
         Environment $twig,
-        RouterInterface $router
+        RouterInterface $router,
+        AuthorizationCheckerInterface $authenticationChecker
     )
     {
         $this->twig = $twig;
         $this->router = $router;
+        $this->authenticationChecker = $authenticationChecker;
     }
 
     /**
@@ -45,5 +54,23 @@ class LegalController
     public function dataProtection()
     {
         return new Response($this->twig->render('legal/data_protection.html.twig'));
+    }
+
+    /**
+     * @Route("/back", name="back", methods={"GET","POST"})
+     * @return Response
+     * @throws Exception
+     */
+    public function back() : Response
+    {
+        if ($this->authenticationChecker->isGranted('IS_AUTHENTICATED_REMEMBERED')) {
+            $url = $this->router->generate('ticket_view_user_list');
+            return new RedirectResponse($url, 302);
+        }else {
+            $url = $this->router->generate('ticket_list');
+            return new RedirectResponse($url, 302);
+        }
+
+        return new Response($content);
     }
 }
